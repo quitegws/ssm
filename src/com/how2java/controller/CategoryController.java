689e3593702b29dc80ce6ee735843b5b948172d4
quitegws <quitegws@gmail.com>
2017-07-20 14:29:21 +0800

    page

--- src/com/how2java/controller/CategoryController.java
+++ src/com/how2java/controller/CategoryController.java
@@ -1,6 +1,5 @@
 package com.how2java.controller;
 
-
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +9,7 @@ import org.springframework.web.servlet.ModelAndView;
 
 import com.how2java.pojo.Category;
 import com.how2java.service.CategoryService;
+import com.how2java.util.Page;
 
 // 告诉spring mvc这是一个控制器类
 @Controller
@@ -19,10 +19,14 @@ public class CategoryController {
 	CategoryService categoryService;
 
 	@RequestMapping("listCategory")
-	public ModelAndView listCategory(){
+	public ModelAndView listCategory(Page page){
+
 		ModelAndView mav = new ModelAndView();
-		List<Category> cs= categoryService.list();
-		
+		List<Category> cs= categoryService.list(page);
+		int total = categoryService.total();
+
+		page.caculateLast(total);
+
 		// 放入转发参数
 		mav.addObject("cs", cs);
 		// 放入jsp路径
--- src/com/how2java/service/impl/CategoryServiceImpl.java
+++ src/com/how2java/service/impl/CategoryServiceImpl.java
@@ -8,15 +8,26 @@ import org.springframework.stereotype.Service;
 import com.how2java.mapper.CategoryMapper;
 import com.how2java.pojo.Category;
 import com.how2java.service.CategoryService;
+import com.how2java.util.Page;
 
 @Service
 public class CategoryServiceImpl  implements CategoryService{
 	@Autowired
 	CategoryMapper categoryMapper;
-	
-	
+
 	public List<Category> list(){
 		return categoryMapper.list();
+	}
+
+	@Override
+	public List<Category> list(Page page) {
+		// TODO Auto-generated method stub
+		return categoryMapper.list(page);
+	}
+
+	@Override
+	public int total() {
+		return categoryMapper.total();
 	};
 
 }
